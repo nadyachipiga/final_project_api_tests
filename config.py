37c5082726238c59c5ef5aefba2bc43a12c81955
Nadya Chipiga <chipiganady@gmail.com>
2025-08-10 11:10:24 +0300

    Отредактировала config

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://11fa6c91-6d8f-47b4-94f9-ef0df6073c1f.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://11fa6c91-6d8f-47b4-94f9-ef0df6073c1f.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
 GET_ORDERS = "/v1/orders/track?t="
